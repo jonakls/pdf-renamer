1e89409b1a307c2c372883724befbf69542bb49d
jonakls <asmot54@hotmail.com>
2022-05-30 00:14:22 -0500

    feat: initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import csv
+import os
+
+root_directory = os.getcwd();
+files_directory = root_directory + str('\\files')
+files_directory_renamed = root_directory + str('\\renamed')
+csv_file = root_directory + str('\\read.csv')
+
+def main():
+    create_directories()
+    files = os.listdir(files_directory)
+    if len(files) == 0:
+        print('Se necesitan archivos en la carpeta files')
+        return
+    with open(csv_file, newline='') as File:
+        reader = csv.reader(File)
+        for row in reader:
+            comparate(row, files)
+
+def comparate(csv_value, files):
+    for file in files:
+        if csv_value[0] == file.split('.')[0]:
+            renamePdf(file, csv_value[1])
+
+def renamePdf(file, new_name):
+    files_renamed = os.listdir(files_directory_renamed)
+    if len(files_renamed) > 0 :
+        print('La carpeta renamed se necesita vacia')
+        return
+    os.rename(files_directory + str('\\') + file, files_directory_renamed + str('\\') + new_name + str('.pdf'))
+
+def create_directories():
+    if not os.path.exists(files_directory):
+        os.mkdir(files_directory)
+    if not os.path.exists(files_directory_renamed):
+        os.mkdir(files_directory_renamed)
+    if not os.path.exists(csv_file):
+        print('Se necesita del archivo csv para iniciar')
+
+main()
